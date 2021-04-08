01613c8817febdd9b7a41709464b3046c785d2fe
KOSIT AROMSAVA <kosit.arom@gmail.com>
2021-04-08 15:28:50 +0700

    แก้ไขเรื่อง version php และปรับแต่ง

--- test_font.php
+++
@@ -1,5 +0,0 @@
-<style> 
-@import url(css/fonts/thsarabunnew.css);
-body{ font-family: 'THSarabunNew', sans-serif; font-size: 0.95em; line-height: 1.7em; background: #e1e1e1; }
-</style>
-ทดสอบภาษาไทยฟอร์นไทยสารบัญ
